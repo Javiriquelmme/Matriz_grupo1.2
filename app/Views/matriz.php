78083d32c92865f859b8dff6fa0c9ed16665392f
swaal <swaalter1@gmail.com>
2022-12-10 23:41:25 -0300

    diseño de obtener largo de matriz

---
+++ app/Views/matriz.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html >
+<html lang="en">
+
+<head>
+
+    <meta http-equiv="Content-Type" content="tex/html;charset=UTF-8">
+
+    <title>Document</title>
+</head>
+
+<body>
+    <form method="post">
+
+
+            <table>
+                <tbody>
+                    <tr>
+                        <td>Ingrese el n&uacute;mero de filas de la matriz A :</td>
+                        <td><input name="filas_a" type="text" size="4"></td>
+                    </tr>
+                    <tr>
+                        <td>Ingrese el n&uacute;mero de columnas de la matriz A :</td>
+                        <td><input name="columnas_a" type="text" size="4"></td>
+                    </tr>
+                    <tr>
+                        <td>Ingrese el n&uacute;mero de filas de la matriz B :</td>
+                        <td><input name="filas_b" type="text" size="4"></td>
+                    </tr>
+                    <tr>
+                        <td>Ingrese el n&uacute;mero de columnas de la matriz B :</td>
+                        <td><input name="columnas_b" type="text" size="4"></td>
+                    </tr>
+                </tbody>
+            </table>
+            <?php
+
+            ?>
+            <input type="submit" value="Continuar">
+    </form>
+
+                </tbody>
+            </table>
+            <?php
+
+
+
+
+        ?>
+
+ <?php
+
+    ?>
+
+
+
+
+
+</body>
+</html>
