71a0667f359f829611f5ce9615f309aefe4c7892
swaal <swaalter1@gmail.com>
2022-12-11 00:57:01 -0300

    prueba de automatización de testeo

--- app/Views/matriz.php
+++ app/Views/matriz.php
@@ -24,7 +24,7 @@
                 unset($_POST['filas_b']);
                 unset($_POST['columnas_b']);
             } else {
-
+                echo "prueba";
        
             }} ?>
             <table>
